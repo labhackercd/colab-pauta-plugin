a766c1ea91f8280327cb6f942be2c7467080e707
Matheus Fernandes <matheus.souza.fernandes@gmail.com>
2017-08-02 11:05:22 -0300

    Added login signals
    Signed-off-by: Matheus Fernandes <matheus.souza.fernandes@gmail.com>

--- src/colab_pauta/apps.py
+++ src/colab_pauta/apps.py
@@ -1,5 +1,6 @@
-
 from colab.plugins.utils.apps import ColabPluginAppConfig
+from colab_pauta.tasks import login_user, logout_user
+from django.contrib.auth.signals import user_logged_in, user_logged_out
 
 
 class ColabPautaAppConfig(ColabPluginAppConfig):
@@ -7,3 +8,7 @@ class ColabPautaAppConfig(ColabPluginAppConfig):
     verbose_name = 'Colab Pauta Plugin'
     short_name = 'colab_pauta'
     namespace = 'colab_pauta'
+
+    def connect_signal(self):
+        user_logged_in.connect(login_user)
+        user_logged_out.connect(logout_user)
