644fe4fcd1da127ade27d72d520d074099a3daea
cfviotti <caio@viotti.me>
2017-10-26 11:38:19 -0200

    Update Pauta proxy template

--- etc/colab/plugins.d/pauta.py
+++ etc/colab/plugins.d/pauta.py
@@ -2,9 +2,16 @@ name = 'colab_pauta'
 verbose_name = 'Colab Pauta Participativa Plugin'
 
 upstream = 'http://127.0.0.1:6000/'
+
 api_key = 'api_key'
 
 urls = {
     'include': 'colab_pauta.urls',
-    'prefix': '^pautas/',
+    'prefix': '^pautaparticipativa/',
+}
+
+settings_variables = {
+    'COLAB_STATICS': [
+        '/colab-plugins/pauta/src/colab_pauta/static'
+    ]
 }
